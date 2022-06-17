e6d2e0d524f8558b33df4162b4ee46c6507b5582
jonas peltzer <jonas.peltzer@stud.hs-ruhrwest.de>
2022-06-17 10:21:11 +0200

    Major changes

--- src/scripts/login.php
+++ src/scripts/login.php
@@ -1,17 +1,17 @@
 <?php 
 session_start();
-$pdo = new PDO('mysql:host=db4free.net;dbname=trikot25', 'testnutzer123', 'mönchengladbach');
+$pdo = new PDO('mysql:host=localhost;dbname=trikot25', 'test', '');
  
 if(isset($_GET['login'])) {
-    $email = $_POST['email'];
-    $passwort = $_POST['passwort'];
+    $mail = $_POST['mail'];
+    $password = $_POST['password'];
     
-    $statement = $pdo->prepare("SELECT * FROM user WHERE email = :email");
-    $result = $statement->execute(array('email' => $email));
+    $statement = $pdo->prepare("SELECT * FROM user WHERE mail = :mail");
+    $result = $statement->execute(array('mail' => $mail));
     $user = $statement->fetch();
         
     //Überprüfung des Passworts
-    if ($user !== false && password_verify($passwort, $user['passwort'])) {
+    if ($user !== false && password_verify($password, $user['password'])) {
         $_SESSION['userid'] = $user['id'];
         die('Login erfolgreich. Weiter zu <a href="index.php">internen Bereich</a>');
     } else {
@@ -35,10 +35,10 @@ if(isset($errorMessage)) {
  
 <form action="?login=1" method="post">
 E-Mail:<br>
-<input type="email" size="40" maxlength="250" name="email"><br><br>
+<input type="mail" size="40" maxlength="250" name="mail"><br><br>
  
 Dein Passwort:<br>
-<input type="password" size="40"  maxlength="250" name="passwort"><br>
+<input type="password" size="40"  maxlength="250" name="password"><br>
  
 <input type="submit" value="Abschicken">
 </form> 
